ea194a98fcc3f78d07b1a07cd60461d1305e49eb
TatianaNefedova <128798008+TatianaNefedova@users.noreply.github.com>
2023-08-08 12:49:49 +0300

    Revert "Feature/reg auth system"

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,9 +21,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reviews.apps.ReviewsConfig',
-    'api.apps.ApiConfig',
-    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
